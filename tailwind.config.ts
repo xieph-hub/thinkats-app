1bcefce1d8ca6339b3d15b13d2cfb2371a6ecf41
xieph-hub <vxiephraim@gmail.com>
2025-11-13 13:36:41 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
+    "./content/**/*.{md,mdx}"
   ],
   theme: {
     extend: {
@@ -15,16 +16,17 @@ const config: Config = {
           green: "#10B981",
           dark: "#0B1220",
           light: "#F5F8FF",
-        },
+          navy: "#172965"
+        }
       },
       boxShadow: {
-        soft: "0 10px 30px rgba(10,102,194,0.12)",
+        soft: "0 10px 30px rgba(10,102,194,0.12)"
       },
       borderRadius: {
-        pill: "9999px",
-      },
-    },
+        pill: "9999px"
+      }
+    }
   },
-  plugins: [],
+  plugins: [typography]
 };
 export default config;
