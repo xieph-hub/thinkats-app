a86969050ceaf61cb1f89935baf5a4d3c6f766a2
xieph-hub <vxiephraim@gmail.com>
2025-11-16 12:55:47 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} min-h-screen bg-slate-50 text-slate-900 antialiased`}
-      >
-        <div className="flex min-h-screen flex-col">
-          <Navbar />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
+    <html lang="en" className={inter.className}>
+      <body className="bg-slate-50 text-slate-900">
+        <Navbar />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
